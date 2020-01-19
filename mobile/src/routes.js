8563a8f51c9e038ace9324793fc21c93362dc189
Alexandre Ramos dos Santos <alexandresantos84@gmail.com>
2020-01-19 14:25:34 -0300

    feat(mobile): create app

---
+++ mobile/src/routes.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main'
+import Profile from './pages/Profile'
+
+const Routes = createAppContainer(
+  createStackNavigator({
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'DevRadar'
+      }
+    },
+    Profile: {
+      screen: Profile,
+      navigationOptions: {
+        title: 'Perfil no GitHub',
+        headerBackTitleVisible: false
+      }
+    }
+  }, {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#7d40e7'
+      },
+      headerTintColor: '#fff'
+
+    }
+  })
+)
+
+export default Routes
